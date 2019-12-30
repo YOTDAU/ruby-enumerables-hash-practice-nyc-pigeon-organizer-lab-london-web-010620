83073c52c14ed1f80b00f52cc5bf21978a1d220b
Simon Tucker <sjtucker28@gmail.com>
2019-12-30 13:19:29 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,3 +2,16 @@ def nyc_pigeon_organizer(data)
   pigeon_list = {}
   data.each do |key, value|
     value.each do |info, name_string|
+      name_string.each do |name|
+        if pigeon_list[name] == nil
+          pigeon_list[name] = {}
+        end
+        if pigeon_list[name][key] == nil
+          pigeon_list[name][key] = []
+        end
+        pigeon_list[name][key] << (info.to_s)
+      end
+    end
+  end
+  pigeon_list
+end
