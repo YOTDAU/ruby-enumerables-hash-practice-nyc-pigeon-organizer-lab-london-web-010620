c5708c8eb2b6a0af89a004c169c647a481e3738d
Simon Tucker <sjtucker28@gmail.com>
2019-12-30 12:57:30 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -8,14 +8,4 @@ def nyc_pigeon_organizer(data)
           :gender => [],
           :lives => []
         }
-        if pigeon_list[name] == nil
-        pigeon_list[name] = {}
-      end
-      if pigeon_list[name][key] == nil
-        pigeon_list[name[key] = []
-      ]
-    pigeon_list[name][key] << (info.to_s)
-  end
-  pigeon_list
-end
-
+        
