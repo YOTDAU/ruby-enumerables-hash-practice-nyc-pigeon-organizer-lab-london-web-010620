e14a9d63fde078a16b44d89ec8af96e62f4b6dbd
Simon Tucker <sjtucker28@gmail.com>
2019-12-30 13:05:42 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,19 +1,4 @@
 def nyc_pigeon_organizer(data)
   pigeon_list = {}
-  data.each do |key, value|
-    value.each do |info, array|
-      array.each do |name|
-        if pigeon_list[name] == nil
-          pigeon_list[name] = {
-          :color => [],
-          :gender => [],
-          :lives => []
-        }
-      end
-    end
-    pigeon_list[name][key].push(info.to_s)
-  end
-  pigeon_list
-end
 
   
